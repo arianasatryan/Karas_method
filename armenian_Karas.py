59812a8213095a7234548d524e875c6840ea322b
arianasatryan <61049885+arianasatryan@users.noreply.github.com>
2020-04-13 03:35:41 +0400

    new splitting,added alpha,'pratt' in test
    New splitting of paragraphs by ':\n' or '.\n' ,cause no whitelines exist in data.
    Used functions are in 'information.py'

--- armenian_Karas.py
+++ armenian_Karas.py
@@ -1,16 +1,14 @@
 import os
-import re
 import json
-import time
 import operator
-from arm_tokenizer import tokenize
+from for_arm.arm_tokenizer import tokenize
 from nltk.tag.perceptron import PerceptronTagger
 from sklearn.feature_extraction.text import TfidfVectorizer
 import scipy.sparse as sp
 import pandas as pd
 from scipy.stats import wilcoxon
-
-start_time = time.time()
+from preprocessing import delete_whitelines_in,simbol_correction_in
+from information import get_starts_of_paragraphs,get_paragraphs_of
 PICKLE = "averaged_perceptron_tagger.pickle"
 
 train_path = '/Users/hekpo/PycharmProjects/Karas/for_arm/data/train_dataset/'
@@ -19,24 +17,6 @@ train_files = os.listdir(train_path)
 test_files = os.listdir(test_path)
 train_results_path = '/Users/hekpo/PycharmProjects/Karas/for_arm/results/train_results/'
 test_results_path = '/Users/hekpo/PycharmProjects/Karas/for_arm/results/test_results/'
-
-
-def get_starts_of_paragraphs(paragraphs):
-    start_of_paragraph = {}
-    i = 1
-    length = 0
-    for item in paragraphs:
-        start_of_paragraph[i] = length
-        i += 1
-        length += (len(item) + 2)
-    return start_of_paragraph
-
-def get_paragraphs_of(file_dir):
-    with open(file_dir, encoding='utf-8')as fin:
-        text = fin.read()
-        white_line_regex = r"(?:\r?\n){2,}"
-        paragraphs = re.split(white_line_regex, text.strip())
-    return paragraphs
 
 # punctuation tokenizer
 punct_list = ['«', '»', '(', ')', '/', '\\',
@@ -74,7 +54,8 @@ def pos_tokenizer(text):
         pos_tokens.append(word_pos[1])
     return pos_tokens
 
-def work_with(file_dir, train_or_test): #train_or_test is a string just to separate results
+def work_with(file_dir, train_or_test,alpha): #train_or_test is a string just to separate results
+    filename, file_format = os.path.splitext(os.path.basename(file_dir))
     paragraphs = get_paragraphs_of(file_dir)
     p = 1
     num_of_paragr = len(paragraphs)
@@ -92,45 +73,48 @@ def work_with(file_dir, train_or_test): #train_or_test is a string just to separ
             paragraphs.append(sents_str)
             num_of_paragr = (len(paragraphs))"""
 
-    start_of_paragraphs = get_starts_of_paragraphs(paragraphs)
-
+    starts_of_paragraphs = get_starts_of_paragraphs(paragraphs)
+    #print(starts_of_paragraphs)
     # 1.word tfidf
     word_vectorizer = TfidfVectorizer(tokenizer=tokenize)
     word_vectors = word_vectorizer.fit_transform(paragraphs)
     # 2.punctation tfidf
     punct_vectorizer = TfidfVectorizer(tokenizer=punct_tokenizer)
     punct_vectors = punct_vectorizer.fit_transform(paragraphs)
-    # 3.stopwords tfidf
-    stopword_vectorizer = TfidfVectorizer(tokenizer=stopword_tokenizer)
-    stopword_vectors = stopword_vectorizer.fit_transform(paragraphs)
-    # 4.3-grams tfidf
-    three_gram_vectorizer = TfidfVectorizer(tokenizer=tokenize, ngram_range=(3, 3))
-    three_gram__vectors = three_gram_vectorizer.fit_transform(paragraphs)
-    # POS tfidf
+    # 3.POS tfidf
     pos_vectorizer = TfidfVectorizer(tokenizer=pos_tokenizer)
     pos_vectors = pos_vectorizer.fit_transform(paragraphs)
+    # 4.stopwords tfidf
+    stopword_vectorizer = TfidfVectorizer(tokenizer=stopword_tokenizer)
+    stopword_vectors = stopword_vectorizer.fit_transform(paragraphs)
+    # 5.3-grams tfidf
+    three_gram_vectorizer = TfidfVectorizer(tokenizer=tokenize, ngram_range=(3, 3))
+    three_gram__vectors = three_gram_vectorizer.fit_transform(paragraphs)
 
-    # concatenating calculated tfidfs
-    vectors = sp.hstack((word_vectors, punct_vectors, stopword_vectors, three_gram__vectors, pos_vectors), format='csr')
+    # used features tf-idfs concatenating
+    vectors = sp.hstack((word_vectors,punct_vectors, pos_vectors, stopword_vectors, three_gram__vectors), format='csr')
+    #vectors = sp.hstack((word_vectors, pos_vectors, stopword_vectors), format='csr')
     denselist = (vectors.todense()).tolist()
     df = pd.DataFrame(denselist)
+    #print(filename,df.shape)
+
     # doing Wilcoxon sign-rank test
     pvalues = {}
     for i in range(num_of_paragr - 1):
-        stat, pvalue = wilcoxon(df.loc[i], df.loc[i + 1], alternative='two-sided')
-        pvalues[i + 1] = pvalue
+        stat, pvalue = wilcoxon(df.iloc[i], df.iloc[i + 1],zero_method='pratt', alternative='two-sided')
+        pvalues[i + 1] = pvalue               #pvalue[i+1] is value for test [i,i+1] in terms of paragraphs between [i-1,i](because of numeration)
+
     # sorting pvalues in increasing  order
     sorted_pvalues = sorted(pvalues.items(), key=operator.itemgetter(1))
 
     # defining % of suspicious parts
     p = 0.3
     S = int(p * num_of_paragr) + 1
-    alpha = 0.05
     style_change_borders = []
     # making a decision whether is a border
     for tpl in (sorted_pvalues[:S]):
         if tpl[1] < alpha:
-            style_change_borders.append(start_of_paragraphs[tpl[0] + 1])  # +1 cause the numeration of paragraphs starts at 1
+            style_change_borders.append(starts_of_paragraphs[tpl[0] + 1])  # +1 cause the numeration of paragraphs starts at 1
     style_change_borders.sort()
 
     # writing results
@@ -143,13 +127,11 @@ def work_with(file_dir, train_or_test): #train_or_test is a string just to separ
     if len(style_change_borders) == 0:
         are_changes_exist = False
 
-    filename, file_format = os.path.splitext(os.path.basename(file_dir))
     with open(output_path + ('%s.truth' % filename), 'w') as o_f:
         # json.dump({"changes":are_changes_exist,"positions": style_change_borders}, o_f, indent=4)
         json.dump({"positions": style_change_borders}, o_f, indent=4)
 
-
-def method_for_files(files):
+def method_for_files(files,alpha):
     if files == train_files:
         path = train_path
         train_or_test = 'train'
@@ -158,8 +140,7 @@ def method_for_files(files):
         train_or_test = 'test'
     for file in files:
         if file.endswith(".txt"):
-            work_with(path + file, train_or_test)
+            work_with(path + file, train_or_test,alpha)
 
-
-method_for_files(test_files)
-print("--- %s seconds ---" % (time.time() - start_time))
+delete_whitelines_in(train_files)
+method_for_files(train_files,alpha=1)
