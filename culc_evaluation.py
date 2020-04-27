cc0071c1033ca4d7ce1070657323d5e16de62ed3
arianasatryan <61049885+arianasatryan@users.noreply.github.com>
2020-04-28 02:50:27 +0400

    calculate evaluation for arm and eng

--- culc_evaluation.py
+++ culc_evaluation.py
@@ -1,12 +1,20 @@
-from pan17_stylebreach_evaluator import main
+from arm_pan17_stylebreach_evaluator import main
 
-train_path='/Users/hekpo/PycharmProjects/Karas/for_arm/data/train_dataset'
-test_path='/Users/hekpo/PycharmProjects/Karas/for_arm/data/test_dataset'
-train_results_path='/Users/hekpo/PycharmProjects/Karas/for_arm/results/train_results'
-test_results_path='/Users/hekpo/PycharmProjects/Karas/for_arm/results/test_results'
-train_output_dir='/Users/hekpo/PycharmProjects/Karas/for_arm/results/train_results'
-test_output_dir='/Users/hekpo/PycharmProjects/Karas/for_arm/results/test_results'
+train_path = '/Users/hekpo/PycharmProjects/Karas/for_arm/data/train_dataset'
+test_path = '/Users/hekpo/PycharmProjects/Karas/for_arm/data/test_dataset'
+train_results_path = '/Users/hekpo/PycharmProjects/Karas/for_arm/results/train_results'
+test_results_path = '/Users/hekpo/PycharmProjects/Karas/for_arm/results/test_results'
+train_output_path = '/Users/hekpo/PycharmProjects/Karas/for_arm/results/train_results'
+test_output_path = '/Users/hekpo/PycharmProjects/Karas/for_arm/results/test_results'
 
-main(train_path,train_results_path,train_output_dir)
+"""
+from eng_pan17_stylebreach_evaluator import main
 
+train_path = '/Users/hekpo/PycharmProjects/Karas/for_eng/data/pan17-style-breach-detection-training-dataset-2017-02-15'
+test_path = '/Users/hekpo/PycharmProjects/Karas/for_eng/data/pan17-style-breach-detection-test-dataset-2017-02-15'
+train_results_path = '/Users/hekpo/PycharmProjects/Karas/for_eng/results/train_results'
+test_results_path = '/Users/hekpo/PycharmProjects/Karas/for_eng/results/test_results'
+"""
 
+main(train_path,train_results_path,train_results_path)
+#main(test_path,test_results_path,test_results_path)
